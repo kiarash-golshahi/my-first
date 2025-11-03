8105c28d1c78712696dbe8924060591954210a6a
Kiarash Golshahi <kiarash.programming2025@gmail.com>
2025-11-03 08:10:27 +0330

    Add some changes to Product model and change its place in the file

--- project/store/models.py
+++ project/store/models.py
@@ -1,33 +1,26 @@
 from django.db import models
 
+# Category is the parent model, so I wrote it first.
+class Category(models.Model):
+    """
+    Categories to sort products in the store.
+    """
+    title = models.CharField(max_length=255)
+
 class Product(models.Model):
     """
-    This model contains the basic attributes of a product which is in the online shop; that means:
-        1. Title
-        2. Price
-        3. Details about the product; its identity and its properties
-        4. Category
-        5. Registration date
-    The seller, as admin, has access to this model and can add or delete new products to the database.
+    Products which will be sold in the store.
     """
-    title = models.CharField(max_length=500)
-    price = models.IntegerField()
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # "DecimalField" works better than "IntegerField" in this case, because decimal numbers are more legal than integers in setting prices.
+    # In this case, the price can only have 10 digits and 2 decimal places.
     details = models.TextField()
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
     # The attribute "category" stands for the objects of "Category" model table.
     # It means that each product comes from a category and a category can contain several products.
     # "on_delete" keyword specifies what will happen if the parent object (created category in "Category" model) is deleted.
     # "models.CASCADE" specifies that all of the products will be deleted from the database if their parent object (category) is deleted.
-    registration_date = models.DateTimeField()
-
-    def __str__(self):
-        return f'{self.title}'
-
-class Category(models.Model):
-    """
-    This model make the seller, as admin, able to create categories to sort the products.
-    """
-    title = models.CharField(max_length=500)
-
-    def __str__(self):
-        return f'{self.title}'
+    # "related_name" makes each Category model object able to have access to its relative Product objects using "category.products.all()".
+    registration_date = models.DateTimeField(auto_now_add=True)
+    # "auto_now_add" saves the datetime when the new product is created in the admin panel and fills this property automatically.
