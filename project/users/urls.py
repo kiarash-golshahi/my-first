d10691e1d64c0636635d8eb20a4888e00d73207d
Kiarash Golshahi <kiarash.programming2025@gmail.com>
2025-11-09 07:30:54 +0330

    Add login URL path

--- project/users/urls.py
+++ project/users/urls.py
@@ -6,4 +6,6 @@ app_name = 'users'
 urlpatterns = [
     # Signup page URL
     path('signup/', views.signup, name='signup'),
+    # Login page URL
+    path('login/', views.login_view, name='login'),
 ]
