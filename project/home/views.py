72419409d66d623fd293e0a553af61f719b615b0
Kiarash Golshahi <kiarash.programming2025@gmail.com>
2025-11-10 07:15:51 +0330

    Add newest_products function

--- project/home/views.py
+++ project/home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from store.models import Product
+
+def newest_products():
+    """A function to get 20 newest products"""
+    return Product.objects.order_by('-registration_date')[:20]
 
 def index(request):
     """Return and render the homepage"""
