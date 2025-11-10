5a048e547b23ad0bf89728e7f05ec3c94a2ef732
Kiarash Golshahi <kiarash.programming2025@gmail.com>
2025-11-10 16:32:19 +0330

    Add search view

--- project/store/views.py
+++ project/store/views.py
@@ -1,6 +1,15 @@
 from .models import Category, Product
 from django.shortcuts import render
 
+def search(request):
+    search_term = request.GET.get('search', '').strip()
+    products = Product.objects.filter(title__icontains=search_term) if search_term else []
+    context = {
+        'search': search_term,
+        'products': products,
+    }
+    return render(request, template_name='store/search-results.html', context=context)
+
 def category_page(request, id):
     category = Category.objects.get(id=id)
     products = category.products.all()
