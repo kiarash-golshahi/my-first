9cf706e2be89ce150ec5f8f1fd7a4b7eb364b243
Kiarash Golshahi <kiarash.programming2025@gmail.com>
2025-11-10 16:17:25 +0330

    Add search URL path

--- project/store/urls.py
+++ project/store/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('details/<int:id>', views.product_details, name='product-details'),
     # Category page URL
     path('category/<int:id>', views.category_page, name='category'),
+    # Search results page URL
+    path('search/', views.search, name='search-results'),
 ]
