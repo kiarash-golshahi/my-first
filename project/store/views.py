c1226668f223ffe8fc0ed6b43b7e79a5dbb10d73
Kiarash Golshahi <kiarash.programming2025@gmail.com>
2025-11-10 07:47:45 +0330

    Add category view

--- project/store/views.py
+++ project/store/views.py
@@ -1,5 +1,14 @@
-from .models import Product
+from .models import Category, Product
 from django.shortcuts import render
+
+def category_page(request, id):
+    category = Category.objects.get(id=id)
+    products = category.products.all()
+    context = {
+        'products': products,
+        'category': category,
+    }
+    return render(request, template_name='store/category.html', context=context)
 
 def product_details(request, id):
     product = Product.objects.get(id=id)
