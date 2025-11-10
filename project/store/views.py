88300226360a18e90112f384c655abab4caeeb69
Kiarash Golshahi <kiarash.programming2025@gmail.com>
2025-11-10 07:03:39 +0330

    Add product-details view

--- project/store/views.py
+++ project/store/views.py
@@ -1,3 +1,9 @@
+from .models import Product
 from django.shortcuts import render
 
-# Create your views here.
+def product_details(request, id):
+    product = Product.objects.get(id=id)
+    context = {
+        'product': product,
+    }
+    return render(request, template_name='store/product-details.html', context=context)
